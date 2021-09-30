245c27725f2a4a268a983b4e728d1d9600b9b139
Barnabás Béres <barnabas.beres@stud.uni-corvinus.hu>
2021-10-01 01:14:01 +0200

    7. pont

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -17,21 +17,18 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             var u = new User()
             {
-                LastName = txtLastName.Text,
-                FirstName = txtFirstName.Text
+                FullName = textBox1.Text
             };
             users.Add(u);
         }
