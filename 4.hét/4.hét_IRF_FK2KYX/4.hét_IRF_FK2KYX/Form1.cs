9a9066efe2cda826f3e9b0b4bc6ca6267f88331f
Barnabás Béres <barnabas.beres@stud.uni-corvinus.hu>
2021-10-08 22:24:19 +0200

    4. / 8. feladat

--- 4.hét/4.hét_IRF_FK2KYX/4.hét_IRF_FK2KYX/Form1.cs
+++ 4.hét/4.hét_IRF_FK2KYX/4.hét_IRF_FK2KYX/Form1.cs
@@ -19,13 +19,16 @@ namespace _4.hét_IRF_FK2KYX
         Excel.Application xlApp;
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
+        //headers osztály szinten
+        //meg values
+
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             CreateExcel();
-            //CreateTable();
+            CreateTable();
             //FormatTable();
         }
 
@@ -105,7 +108,8 @@ namespace _4.hét_IRF_FK2KYX
             xlSheet.get_Range(
                 GetCell(2, 1), 
                 GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
-            
+            //FormatTable();
+
         }
         string GetCell(int x, int y)
         {
@@ -121,5 +125,30 @@ namespace _4.hét_IRF_FK2KYX
             ExcelCoordinate += x.ToString();
             return ExcelCoordinate;
         }
+
+       //void FormatTable()
+       // {
+       //     Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+       //     headerRange.Font.Bold = true;
+       //     headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+       //     headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+       //     headerRange.EntireColumn.AutoFit();
+       //     headerRange.RowHeight = 40;
+       //     headerRange.Interior.Color = Color.LightBlue;
+       //     headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+       //     Excel.Range tableRange = xlSheet.get_Range(GetCell(2, 1),
+       //         GetCell(1 + values.GetLength(0), values.GetLength(1)));
+       //     tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+       //     Excel.Range firstColumn = xlSheet.get_Range(GetCell(2, 1),
+       //         GetCell(1 + values.GetLength(0), 1));
+       //     firstColumn.Font.Bold = true;
+       //     firstColumn.Interior.Color = Color.LightYellow;
+       //     Excel.Range lastColumn = xlSheet.get_Range(GetCell(2, values.GetLength(1)),
+       //         GetCell(1 + values.GetLength(0), values.GetLength(1)));
+       //     lastColumn.Interior.Color = Color.LightGreen;
+       //     lastColumn.NumberFormat = "#\\ ##0.00";
+       // }
+
     }
 }
