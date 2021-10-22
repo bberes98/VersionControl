56e133b17cca02676ea7ba5167ecc2c14565a6b5
Barnabás Béres <barnabas.beres@stud.uni-corvinus.hu>
2021-10-22 22:34:46 +0200

    week05/3

---
+++ 5.hét/5.hét/5.hét/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.hét
+{
+    class PortfolioItem
+    {
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
