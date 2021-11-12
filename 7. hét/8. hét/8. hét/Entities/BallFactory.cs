7341f0b03f97f5f71dbade69a28b64ee25a8c211
Barnabás Béres <barnabas.beres@stud.uni-corvinus.hu>
2021-11-12 23:26:09 +0100

    week 07 / 3 javító

--- 7. hét/8. hét/8. hét/Entities/BallFactory.cs
+++ 7. hét/8. hét/8. hét/Entities/BallFactory.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace _8.hét.Entities
 {
     class BallFactory
     {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
     }
 }
--- 7. hét/8. hét/8. hét/Form1.cs
+++ 7. hét/8. hét/8. hét/Form1.cs
@@ -16,31 +16,5 @@ namespace _8.hét
         {
             InitializeComponent();
         }
-
-        public class Ball : Label
-        {
-            public Ball()
-            {
-                AutoSize = false;
-                Width = 50;
-                Height = Width;
-                Paint += Ball_Paint;
-            }
-
-            private void Ball_Paint(object sender, PaintEventArgs e)
-            {
-                DrawImage(e.Graphics);
-            }
-
-            protected void DrawImage(Graphics g)
-            {
-                g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-            }
-
-            public void MoveBall()
-            {
-                Left += 1;
-            }
-        }
     }
 }
