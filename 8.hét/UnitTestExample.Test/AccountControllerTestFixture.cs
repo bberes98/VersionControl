60126fc234ecfcbc8cd1ee7d99ad01f1728ac7a3
Barnabás Béres <barnabas.beres@stud.uni-corvinus.hu>
2021-11-26 21:06:50 +0100

    3második / week 09

---
+++ 8.hét/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnitTestExample.Abstractions;
+using UnitTestExample.Controllers;
+using UnitTestExample.Entities;
+
+namespace UnitTestExample.Test
+{
+    class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
