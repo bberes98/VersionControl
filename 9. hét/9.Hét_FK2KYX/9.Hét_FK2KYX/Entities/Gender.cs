0dfae3d43738ba08415ffe826bb262321f404ab2
Barnabás Béres <barnabas.beres@stud.uni-corvinus.hu>
2021-12-03 19:48:12 +0100

    week09/2

---
+++ 9. hét/9.Hét_FK2KYX/9.Hét_FK2KYX/Entities/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Hét_FK2KYX.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+
+}
