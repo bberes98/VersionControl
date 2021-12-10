0b2a771f637b59ee9e561d9c2adf5cf1f25f3d78
Barnabás Béres <barnabas.beres@stud.uni-corvinus.hu>
2021-12-10 22:21:01 +0100

    week10/6

--- 10. hét/10. hét_FK2KYX/10. hét_FK2KYX/Form1.cs
+++ 10. hét/10. hét_FK2KYX/10. hét_FK2KYX/Form1.cs
@@ -29,6 +29,22 @@ namespace _10.hét_FK2KYX
             this.Controls.Add(ga);
             //gc.AddPlayer();
             //gc.Start(true);
+
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
